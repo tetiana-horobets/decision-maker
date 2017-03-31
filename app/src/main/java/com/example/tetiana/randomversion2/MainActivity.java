2aa10f216cbb93e74ab5ca77454a82b3d967f027
Tetiana Horobets <tanusha.sts@gmail.com>
2017-03-31 21:02:13 +0200

    are kept data when turning the screen

--- app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
+++ app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.tetiana.randomversion2;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
     Button button3;
     TextView textViewResult;
 
-
-
     List<String> randomValues = new ArrayList<>();
     List<String> randomValuesListTwo = new ArrayList<>();
     Random random = new Random();
@@ -44,10 +38,8 @@ public class MainActivity extends AppCompatActivity {
         textView2 = (TextView) findViewById(R.id.textView2);
         buttonPlus = (Button) findViewById(R.id.buttonPlus);
         button2 = (Button) findViewById(R.id.button2);
-        button3 = (Button) findViewById(R.id.button3);
+        button3 = (Button) findViewById(R.id.randomizeButton);
         textViewResult = (TextView) findViewById(R.id.textViewResult);
-
-
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
@@ -59,32 +51,24 @@ public class MainActivity extends AppCompatActivity {
                         if (inputText.trim().equals("")){
                             break;
                         }
-                        randomValues.add(inputText);
-                        StringBuilder builder = new StringBuilder();
-                        for (String randomValue : randomValues) {
-                            builder.append(randomValue);
-                            builder.append("\n");
-                        }
 
-                        textView.setText(builder.toString());
+                        randomValues.add(inputText);
+                        textView.setText(formatForTextView(randomValues));
                         inputLine.getText().clear();
                         break;
+
+
                     case R.id.button2:
                         String inputTextListTwo = editText.getText().toString();
                         if (inputTextListTwo.trim().equals("")){
                             break;
                         }
                         randomValuesListTwo.add(inputTextListTwo);
-                        StringBuilder builderTwo = new StringBuilder();
-                        for (String randomValueTwo : randomValuesListTwo) {
-                            builderTwo.append(randomValueTwo);
-                            builderTwo.append("\n");
-                        }
-
-                        textView2.setText(builderTwo.toString());
+                        textView2.setText(formatForTextView(randomValuesListTwo));
                         editText.getText().clear();
                         break;
-                    case R.id.button3:
+                    case R.id.randomizeButton:
+
                         Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
                         int numberWords = randomValues.size();
                         int randomNumber = random.nextInt(numberWords);
@@ -106,5 +90,31 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(onClickListener);
         button3.setOnClickListener(onClickListener);
 
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        bundle.putStringArrayList("randomValues", new ArrayList<>(randomValues));
+        bundle.putStringArrayList("randomValues2", new ArrayList<>(randomValuesListTwo));
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle bundle) {
+        List<String> randomValues = bundle.getStringArrayList("randomValues");
+        List<String> randomValues2 = bundle.getStringArrayList("randomValues2");
+
+        textView.setText(formatForTextView(randomValues));
+        textView2.setText(formatForTextView(randomValues2));
+    }
+
+    private String formatForTextView(List<String> strings) {
+        StringBuilder builder = new StringBuilder();
+
+        for (String string : strings) {
+            builder.append(string);
+            builder.append("\n");
+        }
+
+        return builder.toString();
     }
 }
