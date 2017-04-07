1adc33b85199dde2670ca5da92debea3c34ad380
Tetiana Horobets <tanusha.sts@gmail.com>
2017-04-07 16:55:54 +0200

    dynamically created fields

--- app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
+++ app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -32,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.randomizeButton);
 
+        //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
+        final TableLayout linear = (TableLayout) findViewById(R.id.linear);
+
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -42,7 +48,26 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.button2:
-                        addVariant(inputLine, randomValuesListTwo, textView2);
+                        final View view1 = getLayoutInflater().inflate(R.layout.custom_text_view, null);
+                        final TextView text = (TextView) view1.findViewById(R.id.textViewDynamic);
+                        text.setText("Some text" );
+
+                        //добавляем елементы в linearlayout
+                        linear.addView(view1);
+                        Button deleteField = (Button) view1.findViewById(R.id.buttonDelete);
+                        addVariant(inputLine, randomValuesListTwo, text);
+                        deleteField.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view2) {
+                                try {
+                                    //получаем родительский view и удаляем его
+                                    ((TableLayout) view1.getParent()).removeView(view1);
+                                } catch(IndexOutOfBoundsException ex) {
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+
                         break;
                     case R.id.randomizeButton:
                         Random random = new Random();
@@ -74,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         };
+
         buttonPlus.setOnClickListener(onClickListener);
         button2.setOnClickListener(onClickListener);
         button3.setOnClickListener(onClickListener);
@@ -105,4 +131,6 @@ public class MainActivity extends AppCompatActivity {
 
         return builder.toString();
     }
+
+
 }
