74ea6c60a3ab5954d48fdf5353f642f0dd4ae79f
Tetiana Horobets <tanusha.sts@gmail.com>
2017-04-08 10:57:43 +0200

    Introduced list of lists for random words

--- app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
+++ app/src/main/java/com/example/tetiana/randomversion2/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.tetiana.randomversion2;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -19,14 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView2;
     TextView textView;
-    List<String> randomValues = new ArrayList<>();
-    List<String> randomValuesListTwo = new ArrayList<>();
+    List<List<String>> options = new ArrayList<>();
+    View lastView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.myscreen);
 
+        options.add(new ArrayList<String>());
 
         final EditText inputLine = (EditText) findViewById(R.id.inputLine);
         textView = (TextView) findViewById(R.id.textView);
@@ -36,52 +35,60 @@ public class MainActivity extends AppCompatActivity {
         Button button3 = (Button) findViewById(R.id.randomizeButton);
 
         //находим наш linear который у нас под кнопкой add edittext в activity_main.xml
-        final TableLayout linear = (TableLayout) findViewById(R.id.linear);
+        final LinearLayout linear = (LinearLayout) findViewById(R.id.linear);
+        final View firstView = getLayoutInflater().inflate(R.layout.custom_text_view, null);
+        linear.addView(firstView);
+        lastView = firstView;
+
 
         View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 switch (view.getId()) {
                     case R.id.buttonPlus:
-                        addVariant(inputLine,randomValues,textView);
+                        final TextView text111 = (TextView) lastView.findViewById(R.id.textViewDynamic);
+                        addVariant(inputLine, options.get(options.size() - 1), text111);
                         break;
 
                     case R.id.button2:
-                        final View view1 = getLayoutInflater().inflate(R.layout.custom_text_view, null);
-                        final TextView text = (TextView) view1.findViewById(R.id.textViewDynamic);
-                        text.setText("Some text" );
+                            final View view1 = getLayoutInflater().inflate(R.layout.custom_text_view, null);
+                            final TextView text = (TextView) view1.findViewById(R.id.textViewDynamic);
+                        List<String> newList = new ArrayList<>();
+                        options.add(newList);
+                            //добавляем елементы в linearlayout
+                            addVariant(inputLine, newList, text);
+                            linear.addView(view1);
+                            lastView = view1;
 
-                        //добавляем елементы в linearlayout
-                        linear.addView(view1);
                         Button deleteField = (Button) view1.findViewById(R.id.buttonDelete);
-                        addVariant(inputLine, randomValuesListTwo, text);
+
                         deleteField.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view2) {
                                 try {
                                     //получаем родительский view и удаляем его
-                                    ((TableLayout) view1.getParent()).removeView(view1);
+                                    ((LinearLayout) view1.getParent()).removeView(view1);
                                 } catch(IndexOutOfBoundsException ex) {
                                     ex.printStackTrace();
                                 }
                             }
                         });
 
+
                         break;
                     case R.id.randomizeButton:
                         Random random = new Random();
 
                         Intent intent = new Intent(MainActivity.this, ActivityTwo.class);
-                        int numberWords = randomValues.size();
-                        int randomNumber = random.nextInt(numberWords);
-                        String randomWorld = randomValues.get(randomNumber);
-                        intent.putExtra("rezult", randomWorld);
 
-                        int numberWordsTwo = randomValuesListTwo.size();
-                        int randomNumberTwo = random.nextInt(numberWordsTwo);
-                        String randomWorldTwo = randomValuesListTwo.get(randomNumberTwo);
-                        intent.putExtra("rezultTwo", randomWorldTwo);
+                        String sentence = "";
+                        for(List<String> option : options) {
+                            int numberWords = option.size();
+                            int randomNumber = random.nextInt(numberWords);
+                            String randomWord = option.get(randomNumber);
+                            sentence += " " + randomWord;
+                        }
+                        intent.putExtra("rezult", sentence);
 
                         startActivity(intent);
                         break;
@@ -108,17 +115,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onSaveInstanceState(Bundle bundle) {
-        bundle.putStringArrayList("randomValues", new ArrayList<>(randomValues));
-        bundle.putStringArrayList("randomValues2", new ArrayList<>(randomValuesListTwo));
+
     }
 
     @Override
     public void onRestoreInstanceState(Bundle bundle) {
-        List<String> randomValues = bundle.getStringArrayList("randomValues");
-        List<String> randomValues2 = bundle.getStringArrayList("randomValues2");
-
-        textView.setText(formatForTextView(randomValues));
-        textView2.setText(formatForTextView(randomValues2));
     }
 
     private String formatForTextView(List<String> strings) {
